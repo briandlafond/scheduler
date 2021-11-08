22c60c3920539be96cb440f6f54de7a2639e9c56
Brian <brian.d.lafond@gmail.com>
2021-11-08 16:28:04 -0500

    add day list item component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--full": props.spots === 0,
+    "day-list__item--selected": props.selected
+  });
+
+  const formatSpots = function () {
+    let spotsRemaining = '';
+    if (props.spots === 0) {
+      spotsRemaining = 'no spots remaining';
+    } else if (props.spots > 0) {
+      spotsRemaining = `${props.spots}${props.spots === 1 ? ' spot ': ' spots '} remaining`;
+    }
+    return spotsRemaining;
+  };
+
+  return (
+    <li 
+      className = {dayClass} 
+      onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+
+};
