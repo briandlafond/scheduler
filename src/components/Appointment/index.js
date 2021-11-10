5a34dd620f9a5a4918ad6930ff5cfaf370d41ea0
Brian <brian.d.lafond@gmail.com>
2021-11-10 11:09:47 -0500

    add appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "components/Appointment/styles.scss";
+
+
+export default function Appointment(props) {
+
+  const {time} = props;
+
+  return (
+    (time ? <article className="appointment">Appointment at {time}</article> : <article className="appointment">No Appointment</article>)
+  );
+
+
+};
